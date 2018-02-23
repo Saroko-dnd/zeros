b166f045727fb0f82b065605feed95975686165e
saroko-dnd <isaroko@mail.ru>
2018-02-23 11:05:31 +0200

    function was successfully implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-module.exports = function getZerosCount(number) {
-  // your implementation
-}
+const countPowers = (number, numberForExponentiation) => {
+    let result = 0;
+    let power = 1;
+    let expResult = Math.pow(numberForExponentiation, power);
+
+    while (expResult <= number) {
+        result += Math.floor(number / expResult);
+        ++power;
+        expResult = Math.pow(numberForExponentiation, power);
+    }
+
+    return result;
+};
+
+module.exports = number => {
+    let powersOfFive = 0;
+    let powersOfTwo = 0;
+
+    powersOfFive = countPowers(number, 5);
+    if (powersOfFive) {
+        powersOfTwo = countPowers(number, 2);
+    }
+
+    return powersOfFive <= powersOfTwo ? powersOfFive : powersOfTwo;
+};
